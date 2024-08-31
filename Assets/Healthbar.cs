3b0dc449f12852ad2fca10d6d85974d9f9ea7ac4
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2024-08-31 14:49:39 -0700

    Healthbar Updating

--- Assets/Healthbar.cs
+++ Assets/Healthbar.cs
@@ -5,6 +5,8 @@ public class Healthbar : MonoBehaviour
     // Attach to entity at position offset:
     private Entity entity;
     public Vector3 offset;
+    // Update foreground position/scale:
+    [SerializeField] private Transform foreground;
 
     void Start()
     {
@@ -23,5 +25,12 @@ public class Healthbar : MonoBehaviour
         // Attach to entity at position offset:
         transform.position = entity.transform.position + offset;
         transform.rotation = Quaternion.identity;
+
+        // Update foreground localposition/scale:
+        // at 100%, foreground is pos (0,0) and scale (8,1)
+        // at 0%, foreground is pos (-4,0) and scale (0,1)
+        float healthPercent = entity.CurrentHealth / entity.stats.MaxHealth;
+        foreground.localPosition = Vector2.Lerp(new Vector2(-4, 0), Vector2.zero, healthPercent);
+        foreground.localScale = Vector2.Lerp(new Vector2(0, 1), new Vector2(8, 1), healthPercent);
     }
 }
