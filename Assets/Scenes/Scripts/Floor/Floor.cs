2725bfc89e402409f711493383215554fdac9220
catsniffer47 <kimc5546@gmail.com>
2024-09-19 06:32:44 -0700

    Added SceneTransition to Floor SerializedField + Bad Untested Implementations
    I didn't test the code. Please don't judge me. I'm pushing this so I can test it and see its behavior. I don't know how most of this stuff works.

--- Assets/Scenes/Scripts/Floor/Floor.cs
+++ Assets/Scenes/Scripts/Floor/Floor.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Floor : MonoBehaviour
@@ -19,15 +21,21 @@ public class Floor : MonoBehaviour
 
     void Restart()
     {
-        // Restart current floor. Destroys player.
+        // Restart current floor. Destroys player. I don't know how future scenes will be futureproofed so I'm putting in destroy and instantiation lines.
+        Destroy(player);
+        sceneTransition.ReloadScene();
 
     }
 
     void GoToNext()
     {
-        // Restart current floor. Don't reset player.
+        // Restart current floor. DO NOT reset player.
         // TODO: Make gameobject that calls this function.
-
+        GameObject oldPlayer = player; //Save old character, reload scene and replace default character with upgraded character. 
+        sceneTransition.ReloadScene();
+        Destroy(player);
+        player = oldPlayer; //load old player data into player
+        Instantiate(player);
+        player.transform.position = roomSpawner.StartRoom.transform.position;
     }
-
 }
