8808f117f99ab2443ed92f2576ba48c03c65147d
catsniffer47 <kimc5546@gmail.com>
2024-09-19 07:03:35 -0700

    Created collider that calls function if and only if a rigidbody tagged "Player" touches it
    Uses Restart() because GoToNext() is bugged. Working on that next.

--- Assets/Scenes/Scripts/Floor/Floor.cs
+++ Assets/Scenes/Scripts/Floor/Floor.cs
@@ -19,7 +19,7 @@ public class Floor : MonoBehaviour
         player.transform.position = roomSpawner.StartRoom.transform.position;
     }
 
-    void Restart()
+    public void Restart()
     {
         // Restart current floor. Destroys player. I don't know how future scenes will be futureproofed so I'm putting in destroy and instantiation lines.
         Destroy(player);
@@ -27,7 +27,7 @@ public class Floor : MonoBehaviour
 
     }
 
-    void GoToNext()
+    public void GoToNext()
     {
         // Restart current floor. DO NOT reset player.
         // TODO: Make gameobject that calls this function.
---
+++ Assets/Scenes/Scripts/Floor/NextLevelCollider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelCollider : MonoBehaviour
+{
+    [SerializeField] Floor floor;
+    [SerializeField] BoxCollider2D boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("NextLevelCollider Trigger activated!");
+        if (collision.gameObject.tag == "Player")
+        {
+            //floor.GoToNext();
+            floor.Restart();
+        }
+    }
+}
