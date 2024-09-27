8dfe742df36f2a0e84424b5bc382953cc2e012bb
Aubrey Clark <zane.a.s.clark@gmail.com>
2024-09-26 19:23:31 -0700

    renamed player animation to entity animation

--- Assets/Scenes/Scripts/Player/EntityAnimation.cs
+++ Assets/Scenes/Scripts/Player/EntityAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class EntityAnimation : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Animator animator;
