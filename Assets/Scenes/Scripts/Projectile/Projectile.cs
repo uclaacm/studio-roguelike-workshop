1a92fe9adedb3986d57ed0f693636e84729027eb
Aubrey Clark <zane.a.s.clark@gmail.com>
2024-11-03 20:49:51 -0800

    fixup! add player null check for enemy attack

--- Assets/Scenes/Scripts/Projectile/Projectile.cs
+++ Assets/Scenes/Scripts/Projectile/Projectile.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Variables 
+    // Variables
     [SerializeField] float damage = 1f;
     [SerializeField] public string SourceTag;
-    
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
-        if(!other.gameObject.CompareTag(SourceTag))
+
+        if(!other.gameObject.CompareTag(SourceTag) && !other.isTrigger)
         {
             Entity entity = other.GetComponent<Entity>();
 
