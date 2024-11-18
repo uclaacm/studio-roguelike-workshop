abef8a1b174941c03224075ee05d9aff1e5f9687
GinaMackay2014 <113387926+GraceMao2022@users.noreply.github.com>
2024-11-18 13:06:59 -0800

    make DashItem.cs into skeleton code

--- Assets/Scenes/Scripts/Items/Dash/DashItem.cs
+++ Assets/Scenes/Scripts/Items/Dash/DashItem.cs
@@ -23,47 +23,68 @@ public class DashItem : MonoBehaviour
     Entity entity;
     Rigidbody2D rb;
 
-    void Awake(){
+    void Awake()
+    {
         entity = transform.parent.GetComponent<Entity>();
         rb = transform.parent.GetComponent<Rigidbody2D>();
     }
 
-    void OnMove(InputValue value) {
+    void OnMove(InputValue value)
+    {
         var dir = value.Get<Vector2>();
         dashEndDir = dir;
-        if(dashing) return;
-        if(dir == Vector2.zero) return;
+        if (dashing) return;
+        if (dir == Vector2.zero) return;
 
-        if(Time.time < lastMoveTime + doubleTapDelay && lastMoveDir == dir){
-            Dash(dir);
+        // only dash if time between previous movement input and new input is less than 
+        // doubleTapDelay and if inputs were in same direction 
+        if (Time.time < lastMoveTime + doubleTapDelay && lastMoveDir == dir)
+        {
+            // TODO: call Dash() with current direction
+
         }
-        else{
-            lastMoveTime = Time.time;
-            lastMoveDir = dir;
+        else
+        {
+            // TODO: set lastMoveTime to current time
+            // TODO: set lastMoveDir to current direction
+
         }
     }
 
-    void FixedUpdate(){
-        if(dashing){
-            if(Time.time > dashStartTime + dashDuration){
-                StopDash();
+    void FixedUpdate()
+    {
+        if (dashing)
+        {
+            // stop dashing after dash duration is over
+            if (Time.time > dashStartTime + dashDuration)
+            {
+                // TODO: call StopDash()
+
             }
-            else {
-                rb.velocity = dashDir * entity.stats.MovementSpeed * dashSpeedMult;
+            else
+            {
+                // TODO: set rb's velocity to dashDir multiplied by entity.stats.MovementSpeed 
+                // multiplied by dashSpeedMult
+
             }
         }
     }
 
-    void Dash(Vector2 direction){
-        if(Time.time < lastDashTime + dashCooldown) return;
-        dashing = true;
-        dashDir = direction;
-        dashStartTime = Time.time;
+    void Dash(Vector2 direction)
+    {
+        if (Time.time < lastDashTime + dashCooldown) return;
+
+        // TODO: set dashing equal to true
+        // set dashDir to direction
+        // set dashStartTime to current time
+
     }
 
-    void StopDash(){
-        dashing = false;
-        rb.velocity = dashEndDir * entity.stats.MovementSpeed;
-        lastDashTime = Time.time;
+    void StopDash()
+    {
+        // TODO: set dashing to false
+        // set rb's velocity to dashEndDir multiplied by entity.stats.MovementSpeed
+        // set lastDashTime to current time
+
     }
 }
